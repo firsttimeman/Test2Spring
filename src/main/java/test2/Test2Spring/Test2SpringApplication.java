0117ec367a410968f2b77fd32e5001f1e17b074b
sungsanghwa <sangwha0@naver.com>
2025-09-20 19:10:53 +0900

    feat: 프로젝트 초기 세팅 (springboot + JPA + h2)

---
+++ src/main/java/test2/Test2Spring/Test2SpringApplication.java
@@ -0,0 +1,13 @@
+package test2.Test2Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test2/Test2Spring/Test2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package test2.Test2Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
